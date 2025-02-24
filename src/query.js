13dade696f8f1f3a2a982f4f8049eaf3469e3e4a
Lea Verou <lea@verou.me>
2025-02-24 12:15:30 -0500

    Support `getKey()` returning arrays

--- src/query.js
+++ src/query.js
@@ -45,7 +45,7 @@ export default function (palettes, query, options = {}) {
 				let color = palettes[palette][hue][tint];
 
 				let key = getKey.call(palettes, { hue, tint, palette });
-				key = typeof key === "string" ? key : key.join(Query.KEY_JOINER);
+				key = Array.isArray(key) ? key.join(Query.KEY_JOINER) : key;
 				let value = getValue.call(palettes, color, { hue, tint, palette }, used);
 
 				results[key] ??= [];
