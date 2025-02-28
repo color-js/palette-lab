b296b78cec9cdc64b9c30a33ef217d854dd390fe
Lea Verou <lea@verou.me>
2025-02-28 10:10:16 -0500

    Better details

--- src/query.js
+++ src/query.js
@@ -21,9 +21,16 @@ export default function (palettes, query, options = {}) {
 
 	query = new Query(query);
 
-	let { getValue, getKey, filter, caption, stats, sort } = query;
+	let { getValue, by, getKey, filter, caption, stats, sort } = query;
+	let byStr = by ? by + "" : undefined;
 	let results = {};
 	let used = { ...all };
+
+	let onePerRow = {
+		palette: used.palettes.length <= 1 || byStr.includes("palette"),
+		hue: used.hues.length <= 1 || byStr.includes("hue"),
+		tint: used.tints.length <= 1 || byStr.includes("tint"),
+	};
 
 	if (filter) {
 		used = getSubset(used, filter);
@@ -42,7 +49,7 @@ export default function (palettes, query, options = {}) {
 			let tints = used.tints.flatMap(tint => scale.resolveTint(tint, coreTint));
 			// TODO ensure in  all.tints
 
-			for (let tint of tints) {
+			tintloop: for (let tint of tints) {
 				let color = palettes[palette][hue][tint];
 
 				let value = getValue.call(palettes, color, { hue, tint, palette }, used, all);
@@ -66,14 +73,18 @@ export default function (palettes, query, options = {}) {
 								used,
 								all,
 							);
+
 							if (include === false) {
-								continue;
+								continue tintloop;
 							}
 						}
 					}
 				}
 
 				results[key] ??= [];
+				results[key].details ??= {};
+				results[key].details[palette] ??= [];
+				results[key].details[palette].push(onePerRow.tint ? hue : `${hue}-${tint}`);
 				results[key].push(value);
 			}
 		}
@@ -88,9 +99,16 @@ export default function (palettes, query, options = {}) {
 			values = aggregates.excludeOutliers(values, query.excludeOutliers, acc);
 		}
 
+		if (query.minCount && values.length < query.minCount) {
+			delete results[key];
+			continue;
+		}
+
 		if (query.componentMeta?.type === "angle") {
 			values = normalizeAngles(values);
 		}
+
+		let details = results[key].details;
 
 		results[key] = stats.reduce((acc, stat) => {
 			if (!(stat in aggregates)) {
@@ -116,6 +134,10 @@ export default function (palettes, query, options = {}) {
 			return acc;
 		}, acc);
 
+		if (stats.includes("details")) {
+			acc.details = details;
+		}
+
 		// Remove stats not in the stats list
 		for (let stat in results[key]) {
 			if (stat in aggregates && !stats.includes(stat)) {
@@ -123,9 +145,26 @@ export default function (palettes, query, options = {}) {
 			}
 		}
 
-		// Format the rest
-		for (let stat in results[key]) {
-			results[key][stat] = toPrecision(results[key][stat]);
+		// Format the rest and make sure it's in the order specified
+		for (let stat of stats) {
+			let value = results[key][stat];
+			delete results[key][stat];
+
+			if (stat === "details") {
+				let entries = Object.entries(value).map(([palette, colors]) => [
+					palette,
+					colors.join(", "),
+				]);
+				value = entries
+					.map(([palette, colors]) =>
+						onePerRow.palette ? colors : `${palette} (${colors})`)
+					.join(", ");
+			}
+			else {
+				value = toPrecision(value);
+			}
+
+			results[key][stat] = value;
 		}
 	}
 
